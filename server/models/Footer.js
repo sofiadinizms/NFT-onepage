3d7a138457c4403eef4d333ade73af058e528dcc
Lucas <lemmg@cin.ufpe.br>
2020-11-24 18:04:42 -0300

    criacao do footer.jr

---
+++ server/models/Footer.js
@@ -0,0 +1,23 @@
+const Keystone = require('keystone');
+const { Types } = require('keystone/lib/content');
+const Types = Keystone.Field.Types;
+
+const Footer = new Keystone.List('Footers');
+
+Footer.add({
+	link: {
+		type: Types.Text,
+		required: true,
+		initial: true,
+	},
+	address: {
+		type: Types.Text,
+		required: true,
+		initial: true,
+	},
+	phone: {
+		type: Types.Text,
+		required: true,
+		initial: true,
+	}
+});
