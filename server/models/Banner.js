00d407c983f1304120b61ceafea0529387a15d51
Vitoria <vps3@cin.ufpe.br>
2020-11-25 19:36:08 -0300

    Banner.js
    ## What I did
    Created the model of the banner

---
+++ server/models/Banner.js
@@ -0,0 +1,30 @@
+const keystone = require('keystone');
+
+const { Types } = keystone.Field;
+
+const Banner = new keystone.List('Banner');
+
+Banner.add({
+  valueProposition: {
+    type: Types.Text,
+    required: true,
+    initial: true,
+    index: true,
+  },
+
+  valuePropositionDescription: {
+    type: Types.Text,
+    required: true,
+    initial: true,
+    index: true,
+  },
+
+  image: {
+    type: Types.CloudinaryImages,
+    require: true,
+    initial: true,
+    index: true,
+  },
+});
+
+Banner.register();
