7162ccb5177a3962b827ea6553eb4b11f8edbc2e
Lucas <lemmg@cin.ufpe.br>
2020-12-01 17:58:34 -0300

    Hotfix([servicosFooter]): Ajuste da label dos registros do service

--- server/models/Service.js
+++ server/models/Service.js
@@ -1,20 +1,23 @@
 const Keystone = require('keystone');
+const fields = require('keystone/lib/fieldTypes');
 //const { Types } = require('keystone/lib/content');
 const Types = Keystone.Field.Types;
 
 const Service = new Keystone.List('Services');
 
 Service.add({
-	title: {
+	name: {
 		type: Types.Text,
 		required: true,
     initial: true,
-    index: true,
+		index: true,
 	},
 	image: {
     type: Types.CloudinaryImages,
-    require: true,
-	}
+		require: true,
+		index: true,
+		initial: true,
+		singleImage: true,
+	},
 });
-
 Service.register();
