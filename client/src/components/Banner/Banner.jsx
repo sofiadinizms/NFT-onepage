48151409f83690ebf058092a718a5d216bbb0cc4
Vitoria <vps3@cin.ufpe.br>
2020-12-02 14:04:44 -0300

    feature/banner-front
    Fixed some mistakes and finished some details of the banner

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../Assets/logo_name.png'
 //import axios from 'axios';
 
 import './Banner.css';
@@ -7,12 +8,18 @@ function Banner() {
     //React
   return (//HTML
     <section class="banner">
+      <img id="logo" src={Logo} alt="Logo Delta Dental"></img>
       <div class="banner_text">
 
         <h1 id="title">Banner com a proposta de valor da Delta Odonto</h1>
         <h2 id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
         <a id="our_services" href="/">Nossos serviços</a>
 
+      </div>
+      <div class="container">
+        <a data-scroll href="#full">
+      <div class="arrow"></div>
+      </a>
       </div>
 
     </section>
