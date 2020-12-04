04dd00045de50be6bbc16f85bce6d8efd788b196
Lucas <lemmg@cin.ufpe.br>
2020-12-03 22:37:58 -0300

    Bugfix([feature/navbar]): ajuste no tamanho da fonte responsiva e criação dos index.jsx de cada componente para exportação

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,1 +1,4 @@
 export { default as PostList } from './PostList';
+export { default as SideDrawer } from './SideDrawer';
+export { default as Toolbar } from './Toolbar';
+export { default as Backdrop } from './Backdrop';
