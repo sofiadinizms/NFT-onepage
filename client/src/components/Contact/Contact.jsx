6998b10881841da514ca9fc929fe8e58deda10f3
Daniel Silva <djs@cin.ufpe.br>
2020-12-04 21:08:36 -0300

    Feature(front-contact): placeholder animation for desktop version

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -8,13 +8,28 @@ function Contact () {
       <div className="info">
         <h1>Contato</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <input placeholder="Nome"/>
-        <div className="info_client">
-          <input placeholder="E-mail"/>
-          <input placeholder="Telefone"/>
+        <div className="label-float">
+          <input type="text" placeholder=""/>
+          <label>Nome</label>
         </div>
-        <input placeholder="Assunto"/>
-        <input placeholder="Mensagem"/>
+        <div className="info_client">
+          <div className="label-float">
+            <input type="text" placeholder=""/>
+            <label className="email_phone">E-mail</label>
+          </div>
+          <div className="label-float">
+            <input type="text" placeholder=""/>
+            <label className="email_phone">Telefone</label>
+          </div>
+        </div>
+        <div className="label-float">
+          <input type="text" placeholder=""/>
+          <label>Assunto</label>
+        </div>
+        <div className="label-float">
+          <input type="text" placeholder=""/>
+          <label>Mensagem</label>
+        </div>
         <button type="submit">Enviar</button>
       </div>
       <div className="image_delta">
