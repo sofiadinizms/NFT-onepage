c586c5882ec6073bec1ed06e55ea167f7081c986
Lucas <lemmg@cin.ufpe.br>
2020-12-08 18:38:38 -0300

    Feature([feature/slide-OurBlog]): begins of construction the rest of the section

--- client/src/components/OurBlog/OurBlog.jsx
+++ client/src/components/OurBlog/OurBlog.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import OurBlogSlide from '../OurBlogSlide/OurBlogSlide'
 import '../OurBlog/OurBlog.css'
+import Cd01 from '../Assets/cd01.jpg'
+import Cd02 from '../Assets/cd02.jpg'
+import Cd03 from '../Assets/cd03.jpg'
+import Cd04 from '../Assets/cd04.jpg'
 
 function OurBlog(){
 	return(
@@ -8,6 +12,27 @@ function OurBlog(){
 			<div className="tudo">
 				<h1>Nosso blog</h1>
 				<OurBlogSlide />
+				<div className="free-content">
+					<p>Acesse conteúdos gratuitos:</p>
+					<div className="contents">
+						<div className="content">
+							<img src={Cd01} />
+							<p>Nome do conteúdo</p>
+						</div>
+						<div className="content">
+							<img src={Cd02} />
+							<p>Nome do conteúdo</p>
+						</div>
+						<div className="content">
+							<img src={Cd03} />
+							<p>Nome do conteúdo</p>
+						</div>
+						<div className="content">
+							<img src={Cd04} />
+							<p>Nome do conteúdo</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
