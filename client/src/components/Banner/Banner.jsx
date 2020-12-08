5907ea4a7bf2f89c8f5855c58913e02fe86e9c97
Vitoria <vps3@cin.ufpe.br>
2020-12-08 10:14:14 -0300

    hotfix(feature-front):
    Connected the front-end to the back-end

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -16,23 +16,26 @@ function Banner() {
     loadBanners();
   }, [])
 
-  return (//HTML
-    <section class="banner">
-      <img id="logo" src={Logo} alt="Logo Delta Dental"></img>
-      <div class="banner_text">
-
-        <h1 id="title">Banner com a proposta de valor da Delta Odonto</h1>
-        <h2 id="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-        <a id="our_services" href="/">Nossos serviços</a>
-
-      </div>
-      <div class="container">
+  return (
+    <div class="banner">
+      <div className="container">
+		    {banners?.map(({_id, valueProposition, valuePropositionDescription}) => (
+			    <div>
+            <img id="logo" src={Logo} alt="Logo Delta Dental"></img>
+            <div class="banner_text">
+              <h1 id="title">{valueProposition}</h1>
+              <h2 id="description">{valuePropositionDescription}</h2>
+              <a id="our_services" href="/">Nossos serviços</a>
+            </div>
+          </div>
+		    ))}
+        <div class="container">
         <a data-scroll href="#full">
-      <div class="arrow"></div>
-      </a>
+       <div class="arrow"></div>
+        </a>
       </div>
-
-    </section>
+      </div>
+    </div>
   );
 }
 
