6d4bbad66959c909f527bcbf1757698574aea5f4
js-dan <djs@cin.ufpe.br>
2020-12-11 17:16:01 -0300

    Feature(front-footer): adjustments for mobile version

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Image from '../Assets/Deltaodontologia-logo02.png'
+import Logo_delta from '../Assets/Deltaodontologia-logo02.png'
+import Heart from '../Assets/Heart.png'
+import Logo_CITi from '../Assets/logo-citi-preta.png'
 import Instagram_icon from '../Assets/Instagram_icon.svg'
 import Phone_icon from '../Assets/Phone_icon.svg'
 
@@ -21,23 +23,31 @@ function Footer() {
   return (
     <div className="footer">
       {footer?.map(({address, phone, instagram}) => (
-        <div className="footer_infos">
-          <div className="contact_footer">
-            <p>
-              <img src={Phone_icon}/>
-              {phone}
-            </p>
-            <p>
-              <img src={Instagram_icon}/>
-              <a href="https://instagram.com/">{instagram}</a>              
-            </p>
-            <p>Nos acompanhe nas redes sociais!</p>
+        <div className="footer_section">
+          <div className="footer_infos">
+            <div className="contact_footer">
+              <p>
+                <img src={Phone_icon}/>
+                {phone}
+              </p>
+              <p>
+                <img src={Instagram_icon}/>
+                <a href="https://instagram.com/">{instagram}</a>              
+              </p>
+              <p>Nos acompanhe nas redes sociais!</p>
+            </div>                   
+            <div className="adress_credits">
+              <p>Endereço: {address}</p>
+              <div className="credits">
+                <p>Made with and </p>
+                <img src={Heart}/>
+                <p> by</p>
+                <img src={Logo_CITi}/>
+              </div>
+            </div> 
           </div>
           <div className="logo_footer">
-            <img src={Image}/>
-          </div>          
-          <div className="adress_credits">
-            <p>Endereço: {address}</p>
+            <img src={Logo_delta}/>
           </div> 
         </div>
       ))}
