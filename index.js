0e4e8664ef94ec82cf0ab19c3c2c163308a17f09
sofiadinizms <sdms@cin.ufpe.br>
2022-05-09 17:03:45 -0300

    deploy attempt 7

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ keystone.init({
   'admin path': 'admin',
   'auto update': true,
   port: process.env.PORT || 3001,
-  mongo: 'mongodb://projectES:projectES20212@cluster0-shard-00-00.ks88s.mongodb.net:27017,cluster0-shard-00-01.ks88s.mongodb.net:27017,cluster0-shard-00-02.ks88s.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zj9i2u-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongo: process.env.MONGO_URI,
   auth: true,
   'user model': 'User',
-  'cookie secret': 'WatermelonSugarHIGHWatermelonSugarHIGHWatermelonSugarHIIIIIGH',
-  'cloudinary config': 'cloudinary://929557927127856:FkNSnd7BBRs_scD2qFltGd4EcmA@dn9j97xdb',
+  'cookie secret': process.env.COOKIE_SECRET,
+  'cloudinary config': process.env.CLOUDINARY_URL,
 });
 
 keystone.import('./server/models');
