5d3a28fa74f1b6febe68e18f93b86bf7dc88cd22
sofiadinizms <sdms@cin.ufpe.br>
2022-05-18 06:23:14 -0300

    updated the theme

--- client/src/global/styled.d.ts
+++ client/src/global/styled.d.ts
@@ -3,11 +3,8 @@ import 'styled-components';
 declare module 'styled-components'{
   export interface DefaultTheme{
     colors: {
-      salmonPink: string,
-      darkPurple: string,
       white: string,
       lilac: string,
-      mediumPurple: string,
       green400: string,
       green500: string,
       green1000: string
--- client/src/global/theme.ts
+++ client/src/global/theme.ts
@@ -2,11 +2,8 @@ import { DefaultTheme } from "styled-components";
 
 export const Theme:DefaultTheme = {
   colors: {
-    salmonPink: '#F1898A',
-    darkPurple: '#141F52',
     white: '#FBFBFB',
     lilac: '#EFECFB',
-    mediumPurple: '#5F60D8',
     green400: '#EAF4F4',
     green500: '#CCE3DE',
     green1000: '#001405',
